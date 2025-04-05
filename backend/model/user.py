edcef187741fc8fd779cc6cef758e40bcd1e85a3
Tyler Pak <tpak1103@berkeley.edu>
2025-04-04 20:45:49 -0700

    done

--- backend/model/user.py
+++ backend/model/user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, user_id, username, password, gender, skin_type, age):
+    def __init__(self, user_id, username, password, gender, skin_type, age, notifications, email, products, survey_id):
         self.user_id = user_id
         self.username = username
         # Consider hashing for security
@@ -7,12 +7,21 @@ class User:
         self.gender = gender
         self.skin_type = skin_type
         self.age = age
+        self.notifications = notifications
+        self.email = email
+        self.products = products
+        self.survey_id = survey_id
 
     def to_dict(self):
         return {
             "userId": self.user_id,
             "username": self.username,
+            "password": self.password,
             "gender": self.gender,
             "skinType": self.skin_type,
-            "age": self.age
+            "age": self.age,
+            "notifications": self.notifications,
+            "email": self.email,
+            "fav_products": self.products,
+            "survey_id": self.survey_id
         }
